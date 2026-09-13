fa9158e7f8e9beaaf92a6efc096b3cbe26233f6b
change tittle <pan751229@gmail.com>
2026-09-13 14:28:42 +0900

    change tittle

--- app/page.tsx
+++ app/page.tsx
@@ -16,9 +16,9 @@ export default function HomePage() {
   return (
     <>
       <section className="hero">
-        <h1 className="title">오늘의 한 줄</h1>
+        <h1 className="title">오늘의 생각</h1>
         <p className="lead">
-          지나가다 들르셨다면, 오늘 한 줄만 남기고 가세요.
+          한마디 남겨주세요. 우리 함께 공유해요
         </p>
       </section>
 
